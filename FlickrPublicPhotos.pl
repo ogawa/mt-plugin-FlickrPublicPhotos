f4ceeb652297c0549e7ce05b1d01900adc68ef88
Hirotaka Ogawa <hirotaka.ogawa@gmail.com>
2005-05-06 19:56:48 +0000

    Now support 100+ public photos. Add MTFlickrPublicPhotoUploadDate, MTFlickrPublicPhotoTakenDate, MTFlickrPublicPhotoOwnerName tags.
    git-svn-id: file:///Users/ogawa/svn/mt-plugin-FlickrPublicPhotos/trunk@6 b7bee0e5-6241-4529-aa9e-08ae86e46850

--- FlickrPublicPhotos.pl
+++ FlickrPublicPhotos.pl
@@ -1,6 +1,6 @@
 # A plugin for adding "FlickrPublicPhotos" container and related tags
 #
-# Release 0.11 (Mar 6, 2005)
+# Release 0.12 (May 7, 2005)
 #
 # This software is provided as-is. You may use it for commercial or 
 # personal use. If you distribute it, please keep this notice intact.
@@ -15,17 +15,21 @@ eval {
     require MT::Plugin;
     $plugin = new MT::Plugin();
     $plugin->name("FlickrPublicPhotos Plugin");
-    $plugin->description("Add FlickrPublicPhotos container and related tags. Version 0.11");
+    $plugin->description("Add FlickrPublicPhotos container and related tags. Version 0.12");
     $plugin->doc_link("http://as-is.net/hacks/2005/05/flickrpublicphotos_plugin.html");
     MT->add_plugin($plugin);
 };
 
 use MT::Template::Context;
+use MT::Util qw(offset_time_list);
 
 MT::Template::Context->add_container_tag('FlickrPublicPhotos' => \&photos);
 MT::Template::Context->add_tag('FlickrPublicPhotoTitle' => \&photo_title);
 MT::Template::Context->add_tag('FlickrPublicPhotoURL' => \&photo_url);
 MT::Template::Context->add_tag('FlickrPublicPhotoImgURL' => \&photo_img_url);
+MT::Template::Context->add_tag('FlickrPublicPhotoUploadDate' => \&date_upload);
+MT::Template::Context->add_tag('FlickrPublicPhotoTakenDate' => \&date_taken);
+MT::Template::Context->add_tag('FlickrPublicPhotoOwnerName' => \&owner_name);
 
 sub photos {
     my ($ctx, $args) = @_;
@@ -69,6 +73,25 @@ sub photo_img_url {
     $_[0]->stash('flickr_public_photo')->img_url($_[1]->{size} || 't');
 }
 
+sub date_upload {
+    my $args = $_[1];
+    my $t = $_[0]->stash('flickr_public_photo')->date_upload; # epoch format
+    my @ts = $args->{utc} ?
+	gmtime $t : offset_time_list($t, $_[0]->stash('blog_id'));
+    $args->{ts} = sprintf "%04d%02d%02d%02d%02d%02d", $ts[5]+1900, $ts[4]+1, @ts[3,2,1,0];
+    MT::Template::Context::_hdlr_date($_[0], $args);
+}
+
+sub date_taken {
+    my $args = $_[1];
+    $args->{ts} = $_[0]->stash('flickr_public_photo')->date_taken;
+    MT::Template::Context::_hdlr_date($_[0], $args);
+}
+
+sub owner_name {
+    $_[0]->stash('flickr_public_photo')->owner_name;
+}
+
 package MT::Plugin::FlickrPublicPhotos::Photo;
 
 sub new {
@@ -78,8 +101,7 @@ sub new {
 }
 
 sub title {
-    my $this = shift;
-    $this->{title} || '';
+    $_[0]->{title} || '';
 }
 
 sub url {
@@ -94,6 +116,28 @@ sub img_url {
     $size ||= 't';
     my $url = 'http://photos' . $this->{server} . '.flickr.com/' . $this->{id} . '_' . $this->{secret} . '_' . $size . '.jpg';
     $url;
+}
+
+# INPUT/OUTPUT: UNIX epoch format (should be converted to "YYYYMMDDHHMMSS")
+sub date_upload {
+    $_[0]->{dateupload} || '';
+}
+
+# INPUT: MySQL datetime in owner's localtime
+# OUTPUT: "YYYYMMDDHHMMSS" format
+sub date_taken {
+    my $date = $_[0]->{datetaken};
+    if ($date =~ /(\d\d\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d):(\d\d)/) {
+	sprintf "%04d%02d%02d%02d%02d%02d", $1, $2, $3, $4, $5, $6;
+    } elsif ($date =~ /(\d\d\d\d)-(\d\d)/) {
+	sprintf "%04d%02d01000000", $1, $2;
+    } elsif ($date =~ /(\d\d\d\d)/) {
+	sprintf "%04d0101000000", $1;
+    }
+}
+
+sub owner_name {
+    $_[0]->{ownername} || '';
 }
 
 package MT::Plugin::FlickrPublicPhotos::API;
@@ -130,23 +174,35 @@ sub photos {
     my ($uname) = @_;
     my @photos = ();
     my $nsid = $this->resolve_nsid($uname);
-    my $rsp = $this->execute_method('flickr.people.getPublicPhotos',
-				    { user_id => $nsid });
-    die "Flickr request failed: " . $rsp->{error_message} . "\n"
-	unless $rsp->{success} == 1;
-
+    my ($page, $pages) = (1, 0);
     my $xpath = new XML::Parser::Lite::Tree::XPath();
-    $xpath->set_tree($rsp->{tree});
-    my @photoNodes = $xpath->select_nodes('/photos/photo');
-    for my $node (@photoNodes) {
-	my $photo = new MT::Plugin::FlickrPublicPhotos::Photo();
-	$photo->{id} = $node->{attributes}{id};
-	$photo->{nsid} = $nsid;
-	$photo->{secret} = $node->{attributes}{secret};
-	$photo->{server} = $node->{attributes}{server};
-	$photo->{title} = $node->{attributes}{title};
-	push @photos, $photo;
-    }
+    do {
+	my $rsp = $this->execute_method('flickr.people.getPublicPhotos',
+					{ user_id => $nsid,
+					  page => $page,
+					  extras => 'license,date_upload,date_taken,owner_name,icon_server'
+					  });
+	die "Flickr request failed: " . $rsp->{error_message} . "\n"
+	    unless $rsp->{success} == 1;
+	$xpath->set_tree($rsp->{tree});
+	$pages ||= ($xpath->select_nodes('//photos'))[0]->{attributes}{pages};
+	my @photoNodes = $xpath->select_nodes('/photos/photo');
+	for my $node (@photoNodes) {
+	    my $photo = new MT::Plugin::FlickrPublicPhotos::Photo();
+	    $photo->{id} = $node->{attributes}{id};
+	    $photo->{nsid} = $nsid;
+	    $photo->{secret} = $node->{attributes}{secret};
+	    $photo->{server} = $node->{attributes}{server};
+	    $photo->{title} = $node->{attributes}{title};
+	    $photo->{license} = $node->{attributes}{license};
+	    $photo->{dateupload} = $node->{attributes}{dateupload};
+	    $photo->{datetaken} = $node->{attributes}{datetaken};
+	    $photo->{datetakengranularity} = $node->{attributes}{datetakengranularity};
+	    $photo->{ownername} = $node->{attributes}{ownername};
+	    $photo->{iconserver} = $node->{attributes}{iconserver};
+	    push @photos, $photo;
+	}
+    } while ($page++ < $pages);
     return @photos;
 }
 
